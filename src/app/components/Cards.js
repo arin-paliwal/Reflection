c73cdd1cf1c2f2241e47213c57b8e42c10a47e07
Arin Paliwal <paliwalarin09@gmail.com>
2023-06-06 15:57:36 +0530

    Home Page Completed

--- src/app/components/Cards.js
+++ src/app/components/Cards.js
@@ -1,35 +1,55 @@
 import Image from "next/image";
 import logo from "../../../assets/images/logo.png";
 import React from "react";
+import bookmark from "../../../assets/images/bookmark.gif";
 const styles = {
   authorContainer: "flex gap-5 ",
   authorImageContainer: "",
   authorImage: "object-cover",
-  postDetails: "",
+  postDetails: "flex-[2.5]",
+  wrapper: "flex max-width-[46rem] h-[10rem] items-center gap-1rem",
   briefing: "text-[#787878]",
   title: "font-bold text-2xl",
   authorName: "font-semibold",
+  detailsContainer: "flex items-center justify-between text-[#787878]",
+  articleDetails: "my-2 text-[.8rem]",
+  category: "bg-[#d5d5d5] p-2 rounded-full",
+  bookmark: "cursor-pointer",
 };
 const Cards = () => {
   return (
-    <div className={styles.postDetails}>
-      <div className={styles.authorContainer}>
-        <div className={styles.authorImageContainer}>
-          <Image
-            src={logo}
-            alt="logo"
-            className={styles.authorImage}
-            width={40}
-            height={40}
-          />
+    <div className={styles.wrapper}>
+      <div className={styles.postDetails}>
+        <div className={styles.authorContainer}>
+          <div className={styles.authorImageContainer}>
+            <Image
+              src={logo}
+              alt="logo"
+              className={styles.authorImage}
+              width={40}
+              height={40}
+            />
+          </div>
+          <div className={styles.authorName}>Arin Paliwal</div>
         </div>
-        <div className={styles.authorName}>Arin Paliwal</div>
+        <h1 className={styles.title}>
+          The technologies that will lead you to a good package.
+        </h1>
+        <div className={styles.briefing}>
+          The leading market consists of WEB3 and Machine Learning.
+        </div>
+        <div className={styles.detailsContainer}>
+          <div className={styles.articleDetails}>
+            20 September ● 5 min ●{" "}
+            <span className={styles.category}>skills enhancement</span>
+          </div>
+          <span className={styles.bookmark}>
+            <Image src={bookmark} alt="bookmark" width={50} height={50} />
+          </span>
+        </div>
       </div>
-      <h1 className={styles.title}>
-        The technologies that will lead you to a good package.
-      </h1>
-      <div className={styles.briefing}>
-        The leading market consists of WEB3 and Machine Learning.
+      <div className={styles.thumbnail}>
+        <Image alt="thumbnail" src={logo} height={100} width={100} />
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,26 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Cards from "./components/Cards";
+const styles = {
+  cards: "flex flex-col gap-3 p-10 sm:grid-cols-2 md:gap-6 lg:grid-cols-3",
+  container: "max-w-7xl flex-1",
+  main: "flex justify-center",
+  wrapper: "mx-auto",
+};
 export default function Home() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Navbar />
       <Banner />
-      <Cards />
-    </>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.cards}>
+            <Cards />
+            <Cards />
+            <Cards />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
