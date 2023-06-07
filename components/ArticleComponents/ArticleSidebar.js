edf3546030b4210dcee9296b67a9dae3a1ceed65
Arin Paliwal <paliwalarin09@gmail.com>
2023-06-07 14:38:22 +0530

    Progress SideBar

--- components/ArticleComponents/ArticleSidebar.js
+++ components/ArticleComponents/ArticleSidebar.js
@@ -1,7 +1,35 @@
 import React from "react";
-
+import Image from "next/image";
+import avatar from "../../assets/images/avatar.jpg";
+import search from "../../assets/images/search.png";
+const styles = {
+  wrapper: "",
+  accentedButton: "bg-black text-white py-2 px-8 rounded-full",
+  searchBar:
+    "flex items-center h-[2.6rem] px-[1rem] gap-[0.6rem] border rounded-full",
+  search: "border-none outline-none w-full bg-none",
+  authorContainer: "my-[2rem]",
+  authorProfileImageContainer: "w-[5rem] rounded-full overflow-hidden",
+};
 const ArticleSidebar = () => {
-  return <div>ArticleSidebar</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.accentedButton}>Get Unlimited Access</div>
+      <div className={styles.searchBar}>
+        <Image src={search} alt="search-icon" width={20} height={20} />
+        <input
+          className={styles.search}
+          placeholder="being curious ... ?"
+          type="text"
+        />
+      </div>
+      <div className={styles.authorContainer}>
+        <div className={styles.authorProfileImageContainer}>
+          <Image src={avatar} alt="profileImage" width={100} height={100} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ArticleSidebar;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,14 @@
 import ArticleSidebar from "@/components/ArticleComponents/ArticleSidebar";
 import ReadersNav from "@/components/ArticleComponents/ReadersNav";
 import React from "react";
-
+const styles = {
+  wrapper: "flex",
+};
 const Post = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <ReadersNav />
+      <div>Main Article</div>
       <ArticleSidebar />
     </div>
   );
