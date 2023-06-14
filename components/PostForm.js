752d6c969125ae1ce4356f7722138926afdb9448
Arin Paliwal <paliwalarin09@gmail.com>
2023-06-14 14:52:52 +0530

    Completed Write Down Page

--- components/PostForm.js
+++ components/PostForm.js
@@ -7,16 +7,12 @@ import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import React, { useContext } from "react";
 import { useState } from "react";
 const styles = {
-  wrapper: "h-screen w-screen p-10",
+  wrapper: "flex flex-row h-screen w-screen p-10",
   logoContainer: "object-contain w-[200px] mr-10",
-  iconContainer: "object-cover w-[70px] items-center justify-center flex",
-  header: "flex flex-row",
-  firstPart: "flex flex-col px-1 py-2 rounded-[50px]",
-  numering: "text-[#787878] text-xl",
-  logo: "mr-20",
-  titleImg: "text-[#787878]",
-  input: "w-full border-0 outline-none bg-transparent",
-  formContainer:"flex"
+  formContainer: "p-5 ml-[5rem] h-screen scrollabe-y w-[800px] rounded-[50px] flex gap-[180px] flex-col",
+  formHeading:"flex flex-start top-0 text-3xl text-[#a4bc92] justify-center",
+  inputContainer:"flex flex-col",
+  inputBoxes:"flex flex-start space-between gap-10 text-xl  w-full"
 };
 
 const PostForm = () => {
@@ -47,7 +43,106 @@ const PostForm = () => {
           <div className={styles.logoContainer}>
             <Image className={styles.logo} src={logo} alt="logo" />
           </div>
+        </div>
+        <div className={styles.formContainer}>
+          <div className={styles.formHeading}>
+            <h1>Enter Article Title and Briefing</h1>
           </div>
+          <form className="flex flex-col gap-4">
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="title"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Blog Title
+              </label>
+              <input
+                type="text"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="article should make people curious"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="brief"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Briefing
+              </label>
+              <input
+                type="text"
+                id="title"
+                value={brief}
+                onChange={(event) => setBrief(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="brief should be designed in curious way"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="imageUrl"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Banner Image
+              </label>
+              <input
+                type="text"
+                value={bannerImage}
+                onChange={(event) => setBannerImage(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="your image url (sorry we don't support drag n drop)"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="category"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Category of the Article
+              </label>
+              <input
+                type="text"
+                id="title"
+                value={category}
+                onChange={(event) => setCategory(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="the category to which article belongs"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="title"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Read Time
+              </label>
+              <input
+                type="text"
+                value={readTime}
+                onChange={(event) => setReadTime(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="average read time to read your article"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <label
+                htmlFor="mainArticle"
+                className="text-xl text-[#787878] mb-[1rem]"
+              >
+                Write down your article
+              </label>
+              <input
+                type="text"
+                value={article}
+                onChange={(event) => setArticle(event.target.value)}
+                className="flex p-2  rounded-[50px] focus:outline-none focus:border-black-100"
+                placeholder="finally...your article"
+              />
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
